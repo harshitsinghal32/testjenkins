b34e6fd3f42a754c04b51cad26714257bf38d4bd
Harshit Singhal <harshit.singhal@shopclues.com>
2017-04-10 18:15:40 +0530

    second

--- src/main/java/testjenkin.java
+++ src/main/java/testjenkin.java
@@ -1,17 +1,20 @@
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+
+
+import org.apache.log4j.Logger;
+
+
 public class testjenkin
 {
+	private static final Logger logger = Logger.getLogger(testjenkin.class);
     public static void main( String[] args )
     {
+    	logger.error("ITS ALIVE!!!!!!!");
         System.out.println("Hello World!");
     }
     
